64df210c816137d7a4bda183e06fd1d55cc9187b
Eduardo Alonso <eduardo.alonso.73h@gmail.com>
2025-10-08 14:51:35 -0600

    Create the struture project

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ChallengeCompose"
+include(":app")
+include(":feature:home")
+include(":feature:detail")
+include(":core:navigation")
+include(":core:network")
+include(":core:ui_components")
+include(":core:designsystem")
